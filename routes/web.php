5ada4f4f25ec5ae3e61a5452811ca46abe2fcdb4
IsaacGomez10 <isaac.21.10@hotmail.com>
2022-05-02 16:53:59 -0500

    evidencia 1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,46 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('paises', function() {
+    $paises = [
+        "Colombia"=>[
+            "cap" => "Bogota",
+            "mon" => "Peso",
+            "pob" =>51,
+            "ciu" => [
+                "Medellin",
+                "Cali",
+                "Pereira",
+                "Barranquilla"
+            ]
+        ],
+        "Argentina"=>[
+            "cap" => "Buenos Aires",
+            "mon" => "Peso Argentino",
+            "pob" =>45.3,
+            "ciu" => [
+                "Cordoba",
+                "La Matanza",
+                "Santa Fe"
+            ]
+        ],
+        "Japon" => [
+            "cap" => "Tokio",
+            "mon" => "Yen Japones",
+            "pob" => 125,8,
+            "ciu" => [
+                "Kioto",
+                "Yohohama",
+                "Osaka",
+                "Hiroshima",
+                "Kanto",
+                "Toyama"
+            ]
+        ]
+    ];
+
+
+    return view('paises')
+    ->with('paises', $paises);
+});
